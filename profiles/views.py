49410e50fc5f1db5f30943c003477e21325a36e0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2021-09-21 17:48:27 -0400

    Complete file upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,21 @@
+from typing import List
+from profiles.models import UserProfile
+from profiles.forms import ProfileForm
 from django.shortcuts import render
 from django.views import View
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles"
 
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
-
-    def post(self, request):
-        pass
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profile.html"
+    context_object_name = "profiles"
